c094b64fb6424c119ed7cf0c4e5ae07897967a41
Akash Sabale <akashsabale100@gmail.com>
2024-12-09 14:12:52 +0530

    added styling and logic in home page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   resources :trees
   get 'home/index'
+  get 'home/pricing'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
