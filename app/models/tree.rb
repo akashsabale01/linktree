b555f11a08a7399670094cd4beac43cf5a61bb00
Akash Sabale <akashsabale100@gmail.com>
2024-12-09 16:33:51 +0530

    added validations in tree model

--- app/models/tree.rb
+++ app/models/tree.rb
@@ -3,4 +3,10 @@ class Tree < ApplicationRecord
   friendly_id :name, use: :slugged
 
   belongs_to :user
+
+  validates :name, presence: true
+  validates :instagram, presence: true, format: {with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])} \z/, message: "Invalid URL format"}
+  validates :x, presence: true, format: {with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])} \z/, message: "Invalid URL format"}
+  validates :youtube, presence: true, format: {with: /\A#{URI::DEFAULT_PARSER.make_regexp(%w[http https])} \z/, message: "Invalid URL format"}
+
 end
